fbd2ff1063677644492e9ae3746a1168a66c7673
Leonard Kevin McGuire Jr <kmcg3413@gmail.com>
2012-11-19 21:43:17 -0600

    CHANGES TO NATIVE DX IMAGE HANDLING + fixed bugs in DX image handling in native code

--- native/main.c
+++ native/main.c
@@ -3,6 +3,7 @@
 typedef unsigned int 		uint32;
 typedef unsigned long long 	uint64;
 typedef unsigned short		uint16;
+typedef signed short		int16;
 typedef unsigned char		uint8;
 
 int _stdcall DllMain(uint32 hInstance, uint32 reason, uint32 reserved)
@@ -32,11 +33,11 @@ int __declspec(dllexport) _stdcall DXT1_decode(void *in, uint32 l, uint32 w, uin
 	uint16		*pout;
 	uint32		ndx;
 	uint16		c1, c2, c3, c4, _c1, _c4;
-	uint8		c1r, c1g, c1b;
-	uint8		c2r, c2g, c2b;
-	uint8		c3r, c3g, c3b;
-	uint8		c4r, c4g, c4b;
-	uint8		tr, tg, tb;
+	int16		c1r, c1g, c1b;
+	int16		c2r, c2g, c2b;
+	int16		c3r, c3g, c3b;
+	int16		c4r, c4g, c4b;
+	int16		tr, tg, tb;
 	uint32		i;
 	uint8		z;
 	uint32		col, row, x, y;
@@ -53,8 +54,8 @@ int __declspec(dllexport) _stdcall DXT1_decode(void *in, uint32 l, uint32 w, uin
 		// indexing is done in 1/4 of block address 16-bit/64-bit 
 		// so we multiply by 4 so we actually access the first
 		// 16-bit of each block
-		c1 = pin[i * 4 + 0];
-		c4 = pin[i * 4 + 1];
+		c4 = pin[i * 4 + 0];
+		c1 = pin[i * 4 + 1];
 		ndx = pin[i * 4 + 2] | (pin[i * 4 + 3] << 16);
 		_c1 = c1;
 		_c4 = c4;
@@ -68,17 +69,19 @@ int __declspec(dllexport) _stdcall DXT1_decode(void *in, uint32 l, uint32 w, uin
 		c4g = c4 & 0x3f;
 		c4 = c4 >> 6;
 		c4r = c4 & 0x1f;
-		tr = c4r - c1r;
-		tg = c4g - c1g;
-		tb = c4b - c1b;
+		tr = (uint16)(c4r - c1r);
+		tg = (uint16)(c4g - c1g);
+		tb = (uint16)(c4b - c1b);
 		c1 = _c1;
 		c4 = _c4;
 		c2r = (tr / 3) + c1r;
 		c2g = (tg / 3) + c1g;
 		c2b = (tb / 3) + c1b;
-		c3r = ((tr / 3) * 2) + c1r;
-		c3g = ((tg / 3) * 2) + c1g;
-		c3b = ((tb / 3) * 2) + c1b;
+		c3r = (tr / 3 * 2) + c1r;
+		c3g = (tg / 3 * 2) + c1g;
+		c3b = (tb / 3 * 2) + c1b;
+		c2 = (c2r << (6 + 5)) | (c2g << 5) | c2b;
+		c3 = (c3r << (6 + 5)) | (c3g << 5) | c3b;
 		// block width is different than decoded image width
 		y = i / bw;
 		x = i - (y * bw);
@@ -93,21 +96,15 @@ int __declspec(dllexport) _stdcall DXT1_decode(void *in, uint32 l, uint32 w, uin
 			{
 				case 0:
 					pout[row * w + col] = c1;
-					//pout[row * w + col] = (c1r << 16) | (c1g << 8) | c1b;
 					break;
 				case 1:
-					pout[row * w + col] = (c2r << (6 + 5)) | (c2g << 5) | c2b;
-					//pout[row * w + col] = (c2r << 16) | (c2g << 8) | c2b;
-					//pout[row * w + col] = 0;
+					pout[row * w + col] = c2;
 					break;
 				case 2:
-					pout[row * w + col] = (c3r << (6 + 5)) | (c3g << 5) | c3b;
-					//pout[row * w + col] = (c3r << 16) | (c3g << 8) | c3b;
-					//pout[row * w + col] = 0;
+					pout[row * w + col] = c3;
 					break;
 				case 3:
 					pout[row * w + col] = c4;
-					//pout[row * w + col] = (c4r << 16) | (c4g << 8) | c4b;
 					break;				
 			}
 			ndx = ndx >> 2;
