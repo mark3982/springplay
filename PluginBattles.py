81d8edc7ecbad014d9375198a4e55e85dac377d8
Leonard Kevin McGuire Jr <kmcg3413@gmail.com>
2012-11-30 22:28:30 -0600

    ADDED SCROLLBAR TO PLUGINBATTLES AND SOME UI ELEMENTS + added partially working scrollbar to PluginBattle + added some other UI components to PluginBattle

--- PluginBattles.py
+++ PluginBattles.py
@@ -27,6 +27,8 @@ class PluginBattles(Plugin.Plugin):
 	
 	mapsUpdated = False
 	
+	image_cache = {}
+	
 	def sessionCb(self, session, event, args):
 		if event == 'battleopened':
 			args['players'] = []
@@ -82,6 +84,7 @@ class PluginBattles(Plugin.Plugin):
 		self.registerForMenuSelection(item)
 		
 		self.panel = 1
+		self.sbpixel = 0
 		
 		self.wParent.paintHook = self.paintHook
 		
@@ -124,27 +127,7 @@ class PluginBattles(Plugin.Plugin):
 	paneltab = 0
 	page = 0
 	widgets = {}
-	
-	def widgetsDeref(self):
-		for k in self.widgets:
-			for w in self.widgets[k]:
-				w.setParent(None)
-	
-	def widgetsPut(self, theType, obj):
-		if theType not in self.widgets:
-			self.widgets[theType] = []
-		self.widgets[theType].append(obj)
-	
-	def widgetsGet(self, theType):
-		if theType not in self.widgets:
-			self.widgets[theType] = []
-		ws = self.widgets[theType]
 		
-		for w in ws:
-			if w.parent() is None:
-				return w
-		return None
-	
 	# our own local cache of QImage objects so
 	# we do not just constantly create and leave
 	# them to the GC.. being safe here
@@ -170,6 +153,11 @@ class PluginBattles(Plugin.Plugin):
 		# is likely the culprit
 		self.mapsUpdated = True
 	
+	def sbValueChanged(self):
+		self.sbpixel = self.widgets['bs_scroll'].value()
+		self.wParent.repaint()
+		return
+	
 	def updateUI(self):
 		# Battle Rooms
 		if self.paneltab == 1:
@@ -186,25 +174,76 @@ class PluginBattles(Plugin.Plugin):
 			# minimap image, or anything else and we need to redraw
 			self.mapsUpdated = False 
 			
-			# unattach all the widgets in the pool from their parent
-			self.widgetsDeref()
-			
 			painter = QtGui.QPainter(panel)
 			painter.setFont(QtGui.QFont('Tahoma'))
 			fontm = painter.fontMetrics()
 			
-			#w = self.widgetsGet(QtGui.QTreeWidgetItem)
-			#print(w)
+			# create options
+			# - option for show empty
+			# - option for show full
+			# - option for official games
+			# - search box for game description
+			# - button to open new battle
 			
-			# ingame
-			if 'greenman' not in self.image_cache:
-				self.image_cache['greenman'] = QtGui.QImage('./images/greenman.png')
-			# idle
-			if 'blackman' not in self.image_cache:
-				self.image_cache['blackman'] = QtGui.QImage('./images/blackman.png')
-			if 'grayman' not in self.image_cache:
-				self.image_cache['grayman'] = QtGui.QImage('./images/grayman.png')
+			# clear any controls from the other panel
+			widgets = self.widgets
+			
+			for k in widgets:
+				if k.find('bs_') != 0:
+					w = widgets[k]
+					# unref it from the wid
+					w.setParent(None)
+					widgets[k] = None
+			
+			# basically we create them like this so we can easily
+			# clear them if they switch to the current battle panel
+			if 'bs_scroll' not in widgets:
+				widgets['bs_scroll'] = QtGui.QScrollBar(panel)
+				widgets['bs_scroll'].valueChanged.connect(self.sbValueChanged)
 				
+			bs_scroll = widgets['bs_scroll']
+			bs_scroll.move(panel.width() - 15, 50)
+			bs_scroll.resize(15, panel.height() - 50)
+			bs_scroll.show()
+			
+			
+			if 'bs_spanel' not in widgets:
+				bs_spanel = QtGui.QWidget(panel)
+				bs_bl = QtGui.QHBoxLayout()
+				
+				widgets['bs_spanel'] = bs_spanel
+				
+				bs_spanel.move(0, 0)
+				bs_spanel.resize(panel.rect().width(), 50)
+				bs_spanel.setLayout(bs_bl)
+				bs_spanel.show()
+			
+				widgets['bs_newbattle'] = QtGui.QPushButton(panel)
+				widgets['bs_newbattle'].setText('New Battle')
+				widgets['bs_newbattle'].show()
+				bs_bl.addWidget(widgets['bs_newbattle'])
+				
+				
+				widgets['bs_showempty'] = QtGui.QCheckBox(panel)
+				widgets['bs_showempty'].setText('Show Empty')
+				widgets['bs_showempty'].show()
+				bs_bl.addWidget(widgets['bs_showempty'])
+
+				widgets['bs_showfull'] = QtGui.QCheckBox(panel)
+				widgets['bs_showfull'].setText('Show Full')
+				widgets['bs_showfull'].show()
+				bs_bl.addWidget(widgets['bs_showfull'])
+				
+				widgets['bs_nextpage'] = QtGui.QPushButton(panel)
+				widgets['bs_nextpage'].setText('Next Page')
+				widgets['bs_nextpage'].show()
+				bs_bl.addWidget(widgets['bs_nextpage'])
+				
+				widgets['bs_prevpage'] = QtGui.QPushButton(panel)
+				widgets['bs_prevpage'].setText('Prev Page')
+				widgets['bs_prevpage'].show()
+				bs_bl.addWidget(widgets['bs_prevpage'])
+			
 			gyoff = 50
 			
 			gridw = 400
@@ -212,12 +251,16 @@ class PluginBattles(Plugin.Plugin):
 			colcnt = int(w / gridw)
 			rowcnt = int(h / (gridh - gyoff)) + 1
 			
+			
+			widgets['bs_scroll'].setRange(0, rowcnt * gridh)
+			
+			if 'battleimg' not in self.image_cache:
+				self.image_cache['battleimg'] = QtGui.QImage('./images/surprised-skull-2.png')
+			
 			if colcnt < 1:
 				colcnt = 1
 			if rowcnt < 1:
-				rowcnt = 1
-			
-			
+				rowcnt = 1	
 			
 			# battles
 			bc = 0
@@ -225,80 +268,98 @@ class PluginBattles(Plugin.Plugin):
 			off = self.page * (colcnt * rowcnt)
 			for bk in self.battles:
 				b = self.battles[bk]
-				if b['mod'].lower().find('zero-k') < 0:
-					continue
-				if bc >= (off + sz):
-					# too far skip out
-					break
-				if bc >= off:
-					andx = bc - off					# actual index (relative to page)
-					arow = int(andx / colcnt)		# actual row
-					acol = andx - (arow * colcnt)	# actual col
-					ax = acol * gridw				# actual x (col)
-					ay = arow * gridh				# actual y (row)
-					# okay render this to the panel
-					#mapImage = self.mapCacheGet(b['map'])
-					mapImage = MapManager.fetchMiniMapAsync(b['map'], self.mapFetchAsyncCb)
-					if mapImage is not None:
-						painter.drawImage(
-											QtCore.QRect(ax, ay + gyoff, 100, 100), 
-											mapImage, 
-											QtCore.QRect(0, 0, -1, -1)
-						)
-					if acol == colcnt - 1:
-						tw = w - ax
-					else:
-						tw = gridw
-					painter.drawRect(ax, ay + gyoff, tw, gridh)
-					painter.drawText(ax + 110, gyoff + ay + fontm.height() * 0 + 10, 'Mod: %s' % b['mod'])
-					painter.drawText(ax + 110, gyoff + ay + fontm.height() * 1 + 10, 'Desc: %s' % b['desc'])
-					painter.drawText(ax + 110, gyoff + ay + fontm.height() * 2 + 10, 'Map: %s' % b['map'])
-					painter.drawText(ax + 110, gyoff + ay + fontm.height() * 3 + 10, 'Players/Specs: %s/%s' % (len(b['players']) - b['specs'], b['specs']))
-					
-					####################################
-					
-					cntSpecs = b['specs'] 
-					cntInGame = 0
-					for p in b['players']:
-						if p in self.players:
-							if self.players[p]['inGame']:
-								cntInGame = cntInGame + 1
-					cntIdle = len(b['players']) - cntInGame - cntSpecs
-					
-					maxp = b['maxPlayers']
-					
-					perRow = 20
-					
-					c_InGame = QtGui.QColor(0x00, 0xff, 0x00)
-					c_Spec = QtGui.QColor(0x00, 0xff, 0xff)
-					c_Idle = QtGui.QColor(0x00, 0x00, 0x00)
-					c_Empty = QtGui.QColor(0x99, 0x99, 0x99)
-					
-					i = 0
-					while i < maxp:
-						row = int(i / perRow)
-						col = i - (row * perRow)
-						__y = row * 10 + ay + fontm.height() * 4 + 10 + gyoff
-						__x = col * 10 + ax + 110
-						
-						if cntInGame > 0:
-							cntInGame = cntInGame - 1
-							c = c_InGame
-						elif cntSpecs > 0:
-							cntSpecs = cntSpecs - 1
-							c = c_Spec
-						elif cntIdle > 0:
-							cntIdle = cntIdle - 1
-							c = c_Idle
-						else:
-							c = c_Empty
-							
-						painter.fillRect(__x, __y, 7, 7, c)
-						
-						i = i + 1
-					#####################
-					#####################
+
+				#andx = bc - off					# actual index (relative to page)
+				andx = bc
 				bc = bc + 1
+				
+				arow = int(andx / colcnt)		# actual row
+				acol = andx - (arow * colcnt)	# actual col
+				ax = acol * gridw				# actual x (col)
+				ay = arow * gridh				# actual y (row)
+				
+				# above the top
+				if ay + gridh < self.sbpixel:	
+					continue
+				# past the bottom
+				if ay > (self.sbpixel + h):
+					continue
+				# adjust for slider position
+				ay = ay - self.sbpixel
+				
+				# okay render this to the panel
+				#mapImage = self.mapCacheGet(b['map'])
+				mapImage = MapManager.fetchMiniMapAsync(b['map'], self.mapFetchAsyncCb)
+				if mapImage is not None:
+					painter.drawImage(
+										QtCore.QRect(ax, ay + gyoff, 100, 100), 
+										mapImage, 
+										QtCore.QRect(0, 0, -1, -1)
+					)
+				for p in b['players']:
+					if p in self.players:
+						if self.players[p]['inGame']:
+							painter.drawImage(
+												QtCore.QRect(ax, ay + gyoff, 100, 100), 
+												self.image_cache['battleimg'], 
+												QtCore.QRect(0, 0, -1, -1)
+							)
+							break
+				#########################
+				if acol == colcnt - 1:
+					tw = w - ax
+				else:
+					tw = gridw
+				painter.drawRect(ax, ay + gyoff, tw, gridh)
+				painter.drawText(ax + 110, gyoff + ay + fontm.height() * 0 + 10, 'Mod: %s' % b['mod'])
+				painter.drawText(ax + 110, gyoff + ay + fontm.height() * 1 + 10, 'Desc: %s' % b['desc'])
+				painter.drawText(ax + 110, gyoff + ay + fontm.height() * 2 + 10, 'Map: %s' % b['map'])
+				painter.drawText(ax + 110, gyoff + ay + fontm.height() * 3 + 10, 'Players/Specs: %s/%s' % (len(b['players']) - b['specs'], b['specs']))
+				
+				####################################
+				
+				cntSpecs = b['specs'] 
+				cntInGame = 0
+				for p in b['players']:
+					if p in self.players:
+						if self.players[p]['inGame']:
+							cntInGame = cntInGame + 1
+				cntIdle = len(b['players']) - cntInGame - cntSpecs
+				
+				maxp = b['maxPlayers']
+				
+				perRow = 20
+				
+				c_InGame = QtGui.QColor(0x00, 0xff, 0x00)
+				c_Spec = QtGui.QColor(0x00, 0xff, 0xff)
+				c_Idle = QtGui.QColor(0x00, 0x00, 0x00)
+				c_Empty = QtGui.QColor(0x99, 0x99, 0x99)
+				
+				i = 0
+				while i < maxp:
+					row = int(i / perRow)
+					col = i - (row * perRow)
+					__y = row * 10 + ay + fontm.height() * 4 + 10 + gyoff
+					__x = col * 10 + ax + 110
+					
+					if cntInGame > 0:
+						cntInGame = cntInGame - 1
+						c = c_InGame
+					elif cntSpecs > 0:
+						cntSpecs = cntSpecs - 1
+						c = c_Spec
+					elif cntIdle > 0:
+						cntIdle = cntIdle - 1
+						c = c_Idle
+					else:
+						c = c_Empty
+						
+					painter.fillRect(__x, __y, 7, 7, c)
+					
+					i = i + 1
+				#####################
+				continue
+				#####################
 			return
 		return
 		
